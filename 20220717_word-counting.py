833f6490d8b46de831cf78980f9dc4ec20e37592
HyeAnn <happy1004ann@gmail.com>
2022-07-17 23:47:50 +0900

    Fix for postposition

--- 20220717_word-counting.py
+++ 20220717_word-counting.py
@@ -1,5 +1,29 @@
 # https://ranpy4e.slack.com/archives/C03N3FVJQ05/p1658042369568109
 
+def postpositions(word):
+    if word.endswith(pp4):
+        pp = word[-4:]
+        word = word[:-4]
+
+    elif word.endswith(pp3):
+        pp = word[-3:]
+        word = word[:-3]
+
+    elif word.endswith(pp2):
+        pp = word[-2:]
+        word = word[:-2]
+        
+    elif word.endswith(pp1):
+        pp = word[-1]
+        word = word[:-1]
+
+    else:
+        return word
+
+    counts[pp] = counts.get(pp,0) + 1
+    return word
+
+
 #name = input('enter file name: ')
 name = 'C:\\Users\\HyeAnn\\Documents\\codes\\BoostCourse\\KakaoTalk_20220717_1623_19_105_준한.txt'
 handle = open(name, 'r', encoding='UTF8')
@@ -7,6 +31,13 @@ handle = open(name, 'r', encoding='UTF8')
 handle.readline()   # xxx 님과 카카오톡 대화
 handle.readline()   # 저장한 날짜 : xxx
 handle.readline()   #
+
+# https://www.korean.go.kr/front/etcData/etcDataView.do?mn_id=46&etc_seq=61&pageIndex=44
+# postpositions
+pp4 = ('으로부터', '에서부터')
+pp3 = ('으로써', '으로서', '로부터', '에게서', '야말로', '한테서', '에다가', '이나마', '에게로', '는커녕', '라든가', '은커녕', '라든지', '한테로', '마따나')
+pp2 = ('에게', '까지', '부터', '처럼', '로서', '밖에', '한테', '께서', '조차', '에다', '로써', '이든', '이니', '더러', '이며', '에서', '따라', '에의', '마냥', '에로')
+pp1 = ('를', '뿐')
 
 counts = dict()
 for line in handle:
@@ -27,10 +58,16 @@ for line in handle:
     for word in words:
         if word == '이모티콘':
             continue
-        else:
-            counts[word] = counts.get(word,0)+1
+
+        word = postpositions(word)
+        if word == '':
+            continue
+
+        counts[word] = counts.get(word,0) + 1
 
 result = sorted(counts.items(), key = lambda item: item[1], reverse = True)
+
+print(f'Total {len(counts)} words')
 #print(result[:30])
 for i in range(30):
     print(f'# {i+1}: {result[i][0]} ({result[i][1]})')
